3056388a4448164049caeeba2a5aed04b7827d65
eunseob <seobseob0810@naver.com>
2023-11-03 23:21:38 +0900

    feat(Message): 에러 메시지 상수화

---
+++ src/main/java/lotto/Message.java
@@ -0,0 +1,11 @@
+package lotto;
+
+public class Message {
+    public static  String NOT_INTEGER_ERROR = "정수를 입력해주세요.";
+    public static String NEGATIVE_ERROR = "양수를 입력해주세요.";
+    public static String THOUSANDS_ERROR = "1,000원 단위로 입력해주세요.";
+    public static String RANGE_ERROR = "1~45인 숫자를 입력해주세요.";
+
+    public static String ENTER_COST = "구입금애긍ㄹ 입력해 주세요.";
+
+}
